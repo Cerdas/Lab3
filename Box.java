23a5f0e1f3bca940a650de3e632dc278cc411e91
Cerdas <67886757+Cerdas@users.noreply.github.com>
2020-07-20 22:56:29 -0500

    Create Box.java

---
+++ Box.java
@@ -0,0 +1,40 @@
+
+public class Box {
+
+	// Creating size variable in Box class
+	// and initializing at 0
+	static int size = 0;
+
+	// Creating method to print star Box
+	public static void printBox() {
+		
+		// Creating for loop to start box
+		// Creating a column for loop in a row for loop
+		for (int row = 1; row <= size; row++) {
+			for (int col = 1; col <= size; col++){
+				
+				// Placing a star on the beginning and end
+				// of the box using size and spaces in between
+				if ((row == 1) || (row == size) || (col == 1) || (col == size)) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			// Required to make the box by 
+			// moving down the row
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		// Changing size by using dot product
+		Box.size = 5;
+		
+		// Printing star box
+		printBox();
+
+	}
+}
